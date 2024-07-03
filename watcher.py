c7a9f2c3c8177ebaba2c18c8c35aefe5684dca0f
bryant tunbutr <bryantt23@users.noreply.github.com>
2024-07-04 03:19:57 +0800

    print headers

---
+++ watcher.py
@@ -0,0 +1,28 @@
+import time
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
+import subprocess
+
+
+class RunScriptOnChangeHandler(FileSystemEventHandler):
+    def __init__(self, script):
+        self.script = script
+
+    def on_modified(self, event):
+        if event.src_path.endswith(self.script):
+            print(f'{self.script} has been modified, running script...')
+            subprocess.run(['python', self.script])
+
+
+if __name__ == "__main__":
+    script_to_watch = 'sitka_highs.py'
+    event_handler = RunScriptOnChangeHandler(script_to_watch)
+    observer = Observer()
+    observer.schedule(event_handler, path='.', recursive=False)
+    observer.start()
+    try:
+        while True:
+            time.sleep(1)
+    except KeyboardInterrupt:
+        observer.stop()
+    observer.join()
