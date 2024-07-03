394bc441d0e857df74eb5f082272ede484ccf7bc
bryant tunbutr <bryantt23@users.noreply.github.com>
2024-07-04 03:37:00 +0800

    highs & lows

--- sitka_highs.py
+++ sitka_highs.py
@@ -14,21 +14,24 @@ for index, column_header in enumerate(header_row):
     print(index, column_header)
 
 # Extract dates and high temperatures.
-dates, highs = [], []
+dates, highs, lows = [], [], []
 for row in reader:
     current_date = datetime.strptime(row[2], '%Y-%m-%d')
     high = int(row[4])
+    low = int(row[5])
     dates.append(current_date)
     highs.append(high)
+    lows.append(low)
 
 print(highs)
 
 # Plot the high temperatures.
 fig, ax = plt.subplots()
 ax.plot(dates, highs, color='red')
+ax.plot(dates, lows, color='blue')
 
 # Format plot.
-ax.set_title("Daily High Temperatures, 2021", fontsize=24)
+ax.set_title("Daily High and Low Temperatures, 2021", fontsize=24)
 ax.set_xlabel('', fontsize=16)
 fig.autofmt_xdate()
 ax.set_ylabel("Temperature (F)", fontsize=16)
